9087f6b62e2d892354fbb3b316691416ef1e7154
DESKTOP-QAKEJ0P\sunga <sungar78@yahoo.com>
2021-12-13 20:09:51 -0600

    Troubleshot through several issues and now we can login a user and have the website display as such.
    Three scary errors were
    InvalidOperationException: No services for type. When initially adding if logic to _Layout.cshtml
    HTTP ERROR 405. When Account Controller needed to have HttpPosts configured for SignIn, Register and Logout
    Error with clicking LogIn with valid user, but nothing happened (signInManager.IsSignedIn(User) always returned False.) Troubleshot as detailed in _Layout.cshtml, turned out not bad.

--- ToDoList/Controllers/Account.cs
+++ ToDoList/Controllers/Account.cs
@@ -12,6 +12,18 @@ namespace ToDoList.Controllers
 {
     public class Account : Controller
     {
+        private UserManager<User> userManager;
+        private SignInManager<User> signInManager;
+
+
+        // changed from AccountController in Bookstore, different controller name here
+        public Account(UserManager<User> userMngr,
+            SignInManager<User> SignInMngr)
+        {
+            userManager = userMngr;
+            signInManager = SignInMngr;
+        }
+
         [HttpGet]
         public IActionResult LogIn(string returnURL = "")
         {
@@ -21,6 +33,91 @@ namespace ToDoList.Controllers
 
         [HttpGet] //make a new url work this way.. first make a model, then a view, then configure in controller
         public IActionResult Register()
+        {
+            return View();
+        }
+
+        // following up on same issue with Views/Shared/_Layout, now need to make LogIn and register actually work
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // also had to be updated to match our current build
+                // note below is weird looking. the actual file path is (appears to be) ToDoList/Models/DomainModels/User
+                // however this pattern seems to match with bookstore
+                // holy f*** it works!! now will add log-in/log-out
+                var user = new ToDoList.Models.User
+                {
+                    UserName = model.Username,
+                    // below not used currently, commenting out.
+                    //Firstname = model.Firstname,
+                    //Lastname = model.Lastname,
+                    //Email = model.Email
+                };
+                var result = await userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+
+
+        [HttpPost]
+        // semi-obvious. allows log-out, then return to home
+        public async Task<IActionResult> LogOut()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        [HttpPost]
+        // would like to detail a little better in comments exactly what's happenign with these post requests, line by line
+        // ie find the path to the model, find things like the LogIn function and trace what's happening
+        // Login looks to just to be a declared function name here.
+        // things like signInManger seem to be built-ins
+        public async Task<IActionResult> LogIn(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // UserName, Password, ReturnURL and Remember me all components of Models/ViewModels/LoginViewModel
+                var result = await signInManager.PasswordSignInAsync(
+                    model.Username, model.Password, isPersistent: model.RememberMe,
+                    lockoutOnFailure: false);
+
+                if (result.Succeeded)
+                {
+                    if (!string.IsNullOrEmpty(model.ReturnUrl) &&
+                        Url.IsLocalUrl(model.ReturnUrl))
+                    {
+                        // looks like if the string isn't (!) empty, and there's a match then actually login
+                        return Redirect(model.ReturnUrl);
+                    }
+                    else
+                    {
+                        // if log-in isn't successful go back home
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+            // if there isn't a username/password that matches return an error
+            ModelState.AddModelError("", "Invalid username/password.");
+            return View(model);
+        }
+
+        public ViewResult AccessDenied()
         {
             return View();
         }
--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using ToDoList.Models;
 
 namespace ToDoList
@@ -27,6 +28,15 @@ namespace ToDoList
 
             services.AddDbContext<ToDoContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("ToDoContext")));
+
+            // add this
+            // added troubleshooting Dependency Injection Error from Views/Shared/_Layout
+            services.AddIdentity<User, IdentityRole>(options => {
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+            }).AddEntityFrameworkStores<ToDoContext>()
+              .AddDefaultTokenProviders();
         }
 
         // Use this method to configure the HTTP request pipeline.
@@ -37,6 +47,10 @@ namespace ToDoList
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization(); //added from stackoverflow user sign-in troubleshooting
+
 
             app.UseEndpoints(endpoints =>
             {
